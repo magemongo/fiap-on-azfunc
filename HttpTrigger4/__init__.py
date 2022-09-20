83270692c6c2796ec043d866fd22362ee7ab569e
antonio.namur <antonio.namur@gmail.com>
2022-09-20 11:57:35 -0300

    db connection configuracoes

--- HttpTrigger4/__init__.py
+++ HttpTrigger4/__init__.py
@@ -1,7 +1,6 @@
 import azure.functions as func
 
 def main(req: func.HttpRequest, doc: func.Out[func.Document]) -> func.HttpResponse:
-
     request_body = req.get_body()
 
     doc.set(func.Document.from_json(request_body))
