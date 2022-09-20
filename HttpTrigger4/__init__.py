b9c1c5cc1a30e6909d0e3b28e8734a77f190d56d
antonio.namur <antonio.namur@gmail.com>
2022-09-20 10:35:17 -0300

    Teste Az Function in CosmosDB

--- HttpTrigger4/__init__.py
+++ HttpTrigger4/__init__.py
@@ -1,23 +1,9 @@
-import logging
 import azure.functions as func
 
+def main(req: func.HttpRequest, doc: func.Out[func.Document]) -> func.HttpResponse:
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    request_body = req.get_body()
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    doc.set(func.Document.from_json(request_body))
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    return 'OK'
