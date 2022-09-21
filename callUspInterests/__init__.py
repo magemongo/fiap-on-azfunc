9bc172f5d4f55001b4132ea961c977bca7612334
antonio.namur <antonio.namur@gmail.com>
2022-09-21 12:56:22 -0300

    None

--- callUspInterests/__init__.py
+++ callUspInterests/__init__.py
@@ -1,9 +1,6 @@
-from http import client
 import logging
 import azure.functions as func
 import azure.cosmos.cosmos_client as cosmos_client
-import azure.cosmos.exceptions as exceptions
-from azure.cosmos.partition_key import PartitionKey
 import json
 import os
 
@@ -12,10 +9,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     req_body = req.get_json()
-    location = req_body.get("location")
-    lng = req_body.get("lng")
-    lat = req_body.get("lat")
-    radio = req_body.get("radio")
 
     # Cria o Client
     client = cosmos_client.CosmosClient(os.environ["HOST"], {'masterKey': os.environ["MASTER_KEY"]})
@@ -26,7 +19,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         # Execute Stored Procedure
         docs = container.scripts.execute_stored_procedure(
             sproc="uspGetInterests",
-            params=[[location, lng, lat, radio]],
+            params=[req_body],
             partition_key="pontos-interesse"
         )
 
